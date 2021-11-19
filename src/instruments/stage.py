aabdde1952964e9828fc5191a64c30b6e9e858fc
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2021-11-19 13:21:20 -0500

    Updated stage
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

--- src/instruments/stage.py
+++ src/instruments/stage.py
@@ -1,9 +1,13 @@
+from __future__ import annotations
+
 import logging
 import time
 from contextlib import contextmanager
 from typing import Dict, Iterator, Literal, Optional
 
-from com import COM, Command
+from src.utils.com import COM, CmdVerify, Command
+
+from instruments.instruments import Movable
 
 logger = logging.getLogger(__name__)
 
@@ -18,7 +22,19 @@ MODES: Dict[ModeName, Dict[ModeParams, str]] = {
 }
 
 
-class YCommand(Command):
+class BetterXstage(Movable):
+    BAUD_RATE = 9600
+    SERIAL_FORMATTER = lambda x: f"{x}\r"
+
+    HOME = 30000
+    RANGE = (1000, 50000)
+    STEPS_PER_UM = 0.4096
+
+    def __init__(self, port: str) -> None:
+        super().__init__(port)
+
+
+class YCmd(Command):
     SET_POS = staticmethod(lambda x: f"D{x}")
     GO = "G"
     CHECK_POS = "R(IP)"
@@ -27,47 +43,54 @@ class YCommand(Command):
     VELO = staticmethod(lambda x: f"V{x}")
 
 
-class BetterYstage:
-    MIN_Y = int(-7e6)
-    MAX_Y = int(7.5e6)
-    STEPS_PER_UM = 100
-    PREFIX = "1"
-    SUFFIX = "\r\n"
+class YCommandVerify:
+    SET_POS = CmdVerify(YCmd.SET_POS, lambda ver, cmd: ver == cmd)
+    GO = CmdVerify()
 
-    def __init__(self, com_port: str, tolerance: int = 10) -> None:
-        self.com = COM(9600, com_port, logger=logger, prefix="1", suffix="\r\n")
-        self.send = self.com.send
+
+class BetterYstage(Movable):
+    BAUD_RATE = 9600
+    SERIAL_FORMATTER = lambda x: f"1{x}\r\n"
+
+    HOME = 0
+    RANGE = (int(-7e6), int(7.5e6))
+    STEPS_PER_UM = 100
+
+    def __init__(self, com_port: str, range_tol: int = 10) -> None:
+        self.com = COM(self.BAUD_RATE, com_port, logger=logger, formatter=self.SERIAL_FORMATTER)
+        self.send = self.com.repl
 
         self.__mode: Optional[ModeName] = None
-        self.tolerance = tolerance
+        self.range_tol = range_tol
 
     def initialize(self) -> None:
         self.send("Z")  # Initialize Stage
         time.sleep(2)
         self.send("W(EX,0)")  # Turn off echo
         self._mode = "MOVING"
-        self.send("MA")  # Set to absolute position mode
-        self.send("ON")  # Turn Motor ON
-        self.send("GH")  # Home Stage
+        [self.send(x) for x in ["MA", "ON", "GH"]]
+        # Set to absolute position mode
+        # Turn Motor ON
+        # Home Stage
 
     @property
     def position(self) -> int:
-        return int(self.send(YCommand.READ_POS)[1:])
+        return int(self.send(YCmd.READ_POS)[1:])
 
     @position.setter
     def position(self, pos: int) -> None:
-        if not (self.MIN_Y <= pos <= self.MAX_Y):
-            raise ValueError(f"YSTAGE can only be between {self.MIN_Y} and {self.MAX_Y}")
+        if not (self.RANGE[0] <= pos <= self.RANGE[1]):
+            raise ValueError(f"YSTAGE can only be between {self.RANGE[0]} and {self.RANGE[1]}")
 
-        while abs(self.position - pos) > self.tolerance:
-            self.send(YCommand.SET_POS(pos))  # type: ignore[operator]
-            self.send(YCommand.GO)
+        while abs(self.position - pos) > self.range_tol:
+            self.send(YCmd.SET_POS(pos))  # type: ignore[operator]
+            self.send(YCmd.GO)
             while not self.is_in_position:
                 time.sleep(0.1)
 
     @property
     def is_in_position(self) -> bool:
-        return self.com.send(YCommand.CHECK_POS)[1:] == "1"
+        return self.com.repl(YCmd.CHECK_POS)[1:] == "1"
 
     @property
     def _mode(self) -> Optional[ModeName]:
@@ -77,8 +100,8 @@ class BetterYstage:
     def _mode(self, mode: ModeName) -> bool:
         if self.__mode == mode:
             return True
-        self.send(YCommand.GAINS(MODES[mode]["GAINS"]))  # type: ignore[operator]
-        self.send(YCommand.VELO(MODES[mode]["VELO"]))  # type: ignore[operator]
+        self.send(YCmd.GAINS(MODES[mode]["GAINS"]))  # type: ignore[operator]
+        self.send(YCmd.VELO(MODES[mode]["VELO"]))  # type: ignore[operator]
         return True
 
     @contextmanager
