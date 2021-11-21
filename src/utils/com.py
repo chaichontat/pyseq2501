b9d13a3ac131e7667947910d63bb7c7cee123698
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2021-11-21 15:55:33 -0500

    COM in separate thread

--- src/utils/com.py
+++ src/utils/com.py
@@ -5,9 +5,11 @@ from contextlib import contextmanager
 from dataclasses import dataclass, field
 from enum import Enum, unique
 from logging import Logger
+from queue import Queue
 from threading import Thread
-from typing import Callable, Optional
+from typing import Any, Callable, Final, NoReturn, Optional, Union
 
+# from concurrent.futures import ThreadPoolExecutor
 from serial import Serial
 
 # from exceptions import InvalidResponse
@@ -17,19 +19,25 @@ from serial import Serial
 #     cmd: str
 #     verify: Optional[Callable[[str], bool]] = None
 
+try:
+    profile  # type: ignore[has-type]
+except NameError:
+    profile = lambda f: f
 
 # @dataclass(frozen=True)
 # class CmdWithArg:
 #     cmd: Callable[[str], str]
 #     verify: Optional[Callable[[str, str], bool]] = None
 
-Str2Bool = Callable[[str], bool]
-
 
 @unique
 class Command(Enum):
     def __str__(self) -> str:
         return self.value
+
+
+Str2Bool = Callable[[str], bool]
+StrCmd = Union[str, Command]
 
 
 @dataclass
@@ -48,6 +56,12 @@ class SerialWriteFailed(Exception):
     ...
 
 
+class COMJob:
+    def __init__(self, f: Callable[[], Optional[str]], waiting: bool = False) -> None:
+        self.f: Final = f
+        self.waiting: Final = waiting
+
+
 @dataclass
 class COM:
     baud: int
@@ -56,7 +70,10 @@ class COM:
     logger: Optional[Logger] = None
     formatter: Optional[Callable[[str], str]] = None
     timeout: int = 1
-    serial: io.TextIOWrapper = field(init=False)
+    thread: Thread = field(init=False)
+    _q_tx: Queue[COMJob | Callable[[], Any]] = field(init=False)
+    _q_rx: Queue[str] = field(init=False)
+    _executor: ThreadPoolExecutor = field(init=False)
 
     def __post_init__(self) -> None:
         if self.formatter is None:
@@ -65,48 +82,82 @@ class COM:
         s_tx = Serial(self.port_tx, self.baud, timeout=self.timeout)
         s_rx = s_tx if self.port_rx is None else Serial(self.port_rx, self.baud, timeout=self.timeout)
         # Text wrapper around serial port
-        self.serial = io.TextIOWrapper(io.BufferedRWPair(s_tx, s_rx), encoding="ascii", errors="strict")  # type: ignore[arg-type]
+        self._serial = io.TextIOWrapper(io.BufferedRWPair(s_tx, s_rx), encoding="ascii", errors="strict")  # type: ignore[arg-type]
 
-    def send(self, cmd: str | Command, timeout: float = 2, debug: bool = True) -> None:
-        def f():
-            assert self.formatter is not None
-            self.serial.write(self.formatter(str(cmd)))
+        self._q_tx = Queue()
+        self._q_rx = Queue(maxsize=1)  # Block if waiting for a response.
 
-        # Protect against SIG and interrupts.
-        t = Thread(target=f)
-        t.start()
-        t.join(timeout)
-        if t.is_alive():
-            raise SerialWriteFailed
+        # self._executor = ThreadPoolExecutor(max_workers=1)
 
-        if debug and self.logger is not None:
-            self.logger.debug(f"Tx: {cmd:10}")
+        def worker(self) -> NoReturn:
+            while True:
+                f = self._q_tx.get()
+                if isinstance(f, COMJob):
+                    if (resp := f.f()) is not None and f.waiting:
+                        self._q_rx.put(resp)
+                elif callable(f):
+                    f()
+                else:
+                    raise TypeError("Invalid command sent to thread.")
+                self._q_tx.task_done()
 
-    def repl(self, cmd: str | Command) -> str:
-        self.send(cmd, debug=False)
-        self.serial.flush()
-        resp = self.serial.readline().strip()
-        if self.logger is not None:
-            if isinstance(cmd, Command):
-                cmd = cmd.name
-            self.logger.debug(f"Tx: {cmd:10} Rx: {resp:10}")
-        return resp
+        self._thread = Thread(target=worker, args=(self,), daemon=True)
+        self._thread.start()
 
-    # @contextmanager
-    # def _repl(self):
-    #     yield
+    def put_job(self, f: Callable[[], Any]) -> None:
+        self._q_tx.put(f)
 
-    def repl_verify(self, cmdver: CmdVerify, attempts: int = 2) -> str:
-        expected = cmdver.expected(str(cmdver.cmd))
-        for _ in range(attempts):
-            resp = self.repl(cmdver.cmd)
-            if resp == expected:
-                break
+    def _send_for_thread(self, cmd: StrCmd) -> None:
+        assert self.formatter is not None
+        self._serial.write(self.formatter(str(cmd)))
+
+    def send(self, cmd: StrCmd) -> None:
+        def work() -> None:
+            self._send_for_thread(cmd)
             if self.logger is not None:
-                self.logger.debug(f"Verification failed for {cmdver.cmd}. Expected {expected} Got {resp}.")
-        else:
-            raise InvalidResponse(cmdver.cmd, resp)
-        return resp
+                self.logger.debug(f"Tx: {cmd:10}")
+
+        self._q_tx.put(COMJob(work))
+
+    def send_blocking(self, cmd: StrCmd) -> None:
+        self.send(cmd)
+        self._q_tx.join()
+
+    def _repl_for_thread(self, cmd: StrCmd) -> str:
+        self._send_for_thread(cmd)
+        self._serial.flush()
+        return self._serial.readline().strip()
+
+    def repl(self, cmd: str | Command, waiting: bool = False) -> Optional[str]:
+        def work() -> str:
+            resp = self._repl_for_thread(cmd)
+            if self.logger is not None:
+                self.logger.debug(f"Tx: {cmd:10} Rx: {resp:10}")
+            return resp
+
+        self._q_tx.put(COMJob(work, waiting))
+        if waiting:
+            # self._q_tx.join()  # Shouldn't be necessary but just in case.
+            return self._q_rx.get()
+        return None
+
+    def join(self) -> None:
+        self._q_tx.join()
+
+    # def repl_verify(self, cmdver: CmdVerify, waiting: bool = False, attempts: int = 2) -> str:
+    #     def work() -> str:
+    #         expected = cmdver.expected(str(cmdver.cmd))
+    #         for _ in range(attempts):
+    #             resp = self._repl_for_thread(cmdver.cmd)
+    #             if resp == expected:
+    #                 break
+    #             if self.logger is not None:
+    #                 self.logger.debug(f"Verification failed for {cmdver.cmd}. Expected {expected} Got {resp}.")
+    #         else:
+    #             raise InvalidResponse(cmdver.cmd, resp)
+    #         return resp
+
+    #     self._q_tx.put(Job(work, waiting))
 
     # @overload
     # def send(self, cmd: CmdNoArg, arg: None, rep: int) -> str:
