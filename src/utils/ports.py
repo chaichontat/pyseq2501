b632053be7c60f458134e3083d79062ba138b242
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2021-11-22 14:41:16 -0500

    Ports taken from Pyseq

--- src/utils/ports.py
+++ src/utils/ports.py
@@ -52,3 +52,62 @@ ports = {
     "laser2": "COM17",
     "arm9chem": "COM8",
 }
+#%%
+
+import configparser
+
+machine = "HiSeq2500"
+cfg = """[HiSeq2500]
+xstage = IL000001A
+ystage = IL000002A
+pumpa = KLOEHNAA
+pumpb = KLOEHNBA
+valvea24 = VICIA2A
+valvea10 = VICIA1A
+valveb24 = VICIB2A
+valveb10 = VICIB1A
+fpgacommand = IL000004A
+fpgaresponse = IL000005A
+laser1 = IL000006A
+laser2 = IL000007A
+arm9chem = ARM9CHEMA
+"""
+com_names = configparser.ConfigParser()
+com_names.read_string(cfg)
+
+# Get list of connected devices
+import wmi
+
+conn = wmi.WMI()
+devices = conn.CIM_LogicalDevice()
+# Get lists of valid COM ports
+ids = []
+com_ports = []
+for d in devices:
+    try:
+        if "USB Serial Port" in d.caption:
+            ids.append(d.deviceid)
+            caption = d.caption
+            id_start = caption.find("(") + 1
+            id_end = caption.find(")")
+            caption = caption[id_start:id_end]
+            com_ports.append(caption)
+    except:
+        pass
+
+# Match instruments to ports
+matched_ports = {}
+for instrument, com_name in com_names.items(machine):
+    try:
+        ind = [i for i, id in enumerate(ids) if com_name in id]
+        if len(ind) == 1:
+            ind = ind[0]
+        else:
+            print("Multiple COM Port matches for", instrument)
+            raise ValueError
+        matched_ports[instrument] = com_ports[ind]
+    except ValueError:
+        matched_ports[instrument] = None
+        print("Could not find port for", instrument)
+print(matched_ports)
+# %%
