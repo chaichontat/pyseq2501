e42abfe2965d2d861737411e3421b24f3fc86290
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2021-12-26 10:58:23 -0500

    Rename to pyseq2

--- setup.py
+++ setup.py
@@ -3,19 +3,15 @@ from setuptools import setup
 setup(
     name="PySeq2501",
     version="0.0.1",
-    description="A example Python package",
-    url="https://github.com/chaichontat",
+    description="Control the HiSeq 2000/2500",
+    url="https://github.com/chaichontat/pyseq2501",
     author="Chaichontat Sriworart",
-    author_email="shudson@anl.gov",
     license="GPL",
-    packages=["src"],
-    install_requires=[
-        "numpy",
-    ],
+    packages=["pyseq2"],
+    install_requires=[],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Science/Research",
-        "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3.10",
     ],
 )
