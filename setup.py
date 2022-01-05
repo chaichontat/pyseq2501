c66e5fcb5133754fe69a660204269abe56076857
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2022-01-05 13:35:01 -0500

    PEP 517
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,17 +1,4 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name="PySeq2501",
-    version="0.0.1",
-    description="Control the HiSeq 2000/2500",
-    url="https://github.com/chaichontat/pyseq2501",
-    author="Chaichontat Sriworart",
-    license="GPL",
-    packages=["pyseq2"],
-    install_requires=[],
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Science/Research",
-        "Programming Language :: Python :: 3.10",
-    ],
-)
+if __name__ == "__main__":
+    setuptools.setup()
