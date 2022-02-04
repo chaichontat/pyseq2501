6309d9f04f90cc1f7c5ed1555026797cc3d50bd0
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2022-02-04 13:08:34 -0500

    Experiment file format

--- pyseq2/experiment.py
+++ pyseq2/experiment.py
@@ -1,14 +1,29 @@
-import json
-from typing import Literal, Sequence, get_args
+from __future__ import annotations
+
+from typing import Annotated, Any, Awaitable, Callable, Literal, Optional, Protocol, Sequence
 
 import yaml
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, Field, root_validator, validator
 
-from pyseq2.flowcell import Seconds, μL, μLpermin
+from pyseq2.flowcell import FlowCells, Seconds, μL, μLpermin
+from pyseq2.fluidics.valve import ReagentPorts
+from pyseq2.imager import Imager
+from pyseq2.utils.utils import until
 
 
 def default_v_pull() -> μLpermin:
     return 100
+
+
+class Pumpable(Protocol):
+    flowcell: Literal[0, 1]
+    port: ReagentPorts
+    v_pull: μLpermin = 100
+    v_push: μLpermin = 2000
+
+
+class Waitable(Protocol):
+    t: Seconds
 
 
 class Reagent(BaseModel):
@@ -36,11 +51,127 @@ class Reagent(BaseModel):
         return port
 
 
-class Command(BaseModel):
-    name: Literal["prime", "pump", "flow", "autofocus", "initialize", "image", "temp", "wait"]
-    flowcell: int
-    reagent: Reagent
+async def wait(
+    flowcells: FlowCells,
+    imager: Imager,
+    f: Callable[[], Awaitable],
+    attempts: int = 120,
+    gap: int | float = 1,
+) -> None:
+    u = 2**5
+    await until(f, attempts=attempts, gap=gap)
+
+
+async def pump(flowcells: FlowCells, imager: Imager, i: Literal[0, 1], r: Reagent, vol: μL):
+    await flowcells[i].flow(r.port, vol, v_pull=r.v_pull, v_push=r.v_push, wait=r.wait)
+
+
+async def prime(flowcells: FlowCells, imager: Imager, i: Literal[0, 1], r: Reagent, vol: μL):
+    await flowcells[i].flow(r.port, vol, v_pull=r.v_prime, v_push=r.v_push, wait=r.wait)
+
+
+async def image(flowcells: FlowCells, imager: Imager, i: Literal[0, 1]):
+    ...
+
+
+class Pump(BaseModel):
+    reagent: Reagent | str
     volume: μL = 250
+    op: Literal["pump"] = "pump"
+
+
+class Prime(BaseModel):
+    reagent: Reagent | str
+    volume: μL = 250
+    op: Literal["prime"] = "prime"
+
+
+class Temp(BaseModel):
+    temp: int | float
+    wait: bool = False
+    op: Literal["temp"] = "temp"
+
+
+class Wait(BaseModel):
+    time: Seconds
+    temp: float
+    op: Literal["wait"] = "wait"
+
+
+class Autofocus(BaseModel):
+    op: Literal["autofocus"] = "autofocus"
+
+
+class Image(BaseModel):
+    xy_start: tuple[float, float]
+    xy_end: tuple[float, float]
+    z_tilt: int
+    channels: frozenset[Literal[0, 1, 2, 3]] = frozenset((0, 1, 2, 3))
+    lasers: tuple[int, int]
+    autofocus: bool = True
+    op: Literal["image"] = "image"
+
+
+Command = Annotated[Pump | Prime | Temp | Wait | Autofocus | Image, Field(discriminator="op")]
+
+
+class Experiment(BaseModel):
+    name: str
+    flowcell: Literal[0, 1]
+    reagents: Optional[dict[str, Reagent]] = None
+    ops: Sequence[Command]  # One level tree for simultaneous events.
+
+    def __init__(self, name: str, flowcell: Literal[0, 1], **data) -> None:
+        super().__init__(name=name, flowcell=flowcell, **data)
+
+    @root_validator
+    def check_reagents(cls, values: dict[str, Any]) -> dict[str, Any]:
+        ops, reagents = values["ops"], values["reagents"]
+
+        # rs: dict[str, Reagent] = dict()
+        for op in ops:
+            try:
+                if isinstance(r := op.reagent, str):
+                    if reagents is None:
+                        raise ValueError(f"No global reagent manifest but have reagent {r} as string.")
+                    if r not in reagents:  # type: ignore
+                        raise ValueError(f"Unknown reagent {r} not in manifest.")  # type: ignore
+                    op.reagent = reagents[r]
+
+                else:
+                    r: Reagent
+                    if (r_ := reagents.get(r.name, None)) is not None and r_ != r:
+                        raise ValueError(f"Reagent {r.name} has inconsistent properties. {r} != {r_}.")
+                    reagents[r.name] = r
+
+            except AttributeError:
+                ...
+
+        values["reagents"] = None
+        return values
+
+    def dict(self, **kwargs):
+        return super(Experiment, self._combi()).dict(**kwargs)
+
+    def _combi(self) -> Experiment:
+        new = Experiment.parse_obj(super(Experiment, self).dict())
+        reagents = dict()
+        for op in new.ops:
+            try:
+                if isinstance(r := op.reagent, Reagent):  # type: ignore
+                    if (saved := reagents.get(r.name, None)) is not None and saved != r:
+                        raise ValueError(f"Reagent {r.name} has inconsistent properties. {r} != {saved}.")
+                    reagents[r.name] = r
+                    op.reagent = r.name
+            except AttributeError:
+                ...
+
+        if new.reagents is None:
+            new.reagents = reagents
+        else:
+            new.reagents.update(reagents)
+
+        return new
 
     @validator("flowcell")
     def fc_check(cls, fc: int):
@@ -48,20 +179,14 @@ class Command(BaseModel):
         return fc
 
 
-class Experiment(BaseModel):
-    name: str
-    ops: Sequence[Sequence[Command] | Command]  # One level tree for simultaneous events.
-
-
 if __name__ == "__main__":
+    # Flush ports 1, 2, 3 with 250 μL per barrel simultaneously.
+    waters = [Reagent(name=f"water{port}", port=port) for port in (1, 2, 3)]
+    ops: list[Command] = [Pump(reagent=water) for water in waters]
+    ops.append(Autofocus())
 
-    # Flush ports 1, 2, 3 of both flowcells with 250 μL per barrel simultaneously.
-    waters = [Reagent(name="water", port=port) for port in (1, 2, 3)]
-    ops = [[Command(name="pump", flowcell=i, reagent=water) for i in (0, 1)] for water in waters]
+    experiment = Experiment("wash_all_ports", 0, ops=ops)
+    print(yaml.dump(experiment._combi().dict()))
 
-    experiment = Experiment(name="wash_all_ports", ops=ops)
-    print(experiment.json())
-    print(yaml.dump(experiment.dict()))
-
-    assert Experiment.parse_obj(json.loads(experiment.json())) == experiment
+    print(Experiment.parse_obj(yaml.safe_load(yaml.dump(experiment.dict()))))
     assert Experiment.parse_obj(yaml.safe_load(yaml.dump(experiment.dict()))) == experiment
