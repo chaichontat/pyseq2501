44eff31e4f1aadacd2d17350c9de60c5c103cfac
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2022-02-22 16:47:18 -0500

    Tailwind 3  config
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 const config = {
   mode: "jit",
-  purge: ["./src/**/*.{html,js,svelte,ts}"],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
     extend: {
