00b7840e89a9e7ab01ba920267999fc6d8d588f4
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2022-02-28 12:26:03 -0500

    Add tests
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

---
+++ tests/test_experiment.py
@@ -0,0 +1,47 @@
+import yaml
+
+from pyseq2.experiment import Experiment
+from pyseq2.experiment.command import Autofocus, Cmd, Goto, Pump, Temp
+from pyseq2.experiment.reagent import Reagent, ReagentGroup, Reagents
+
+
+def test_basic():
+    # Flush ports 1, 2, 3 with 250 μL per barrel.
+    waters: Reagents = [Reagent(name=f"water{port}", port=port) for port in (1, 2, 3)]
+    ops: list[Cmd] = [Pump(reagent=water.name) for water in waters]
+    ops.append(Autofocus(channel=0, laser_onoff=True, laser=5, od=0))
+    ops.append(Temp(temp=25))
+
+    experiment = Experiment("wash_ports_123", False, path=".", cmds=ops, reagents=waters)
+    assert Experiment.parse_raw(experiment.json()) == experiment
+    assert Experiment.parse_obj(yaml.safe_load(yaml.dump(experiment.dict()))) == experiment
+
+
+def test_compile():
+    n = 4
+    mix: Reagents = []
+    mix.append(Reagent(name="water", port=14))
+    mix.append(ReagentGroup(name="gr"))
+    mix += (antibodies := [Reagent(name=f"antibody{port}", port=port) for port in range(1, n + 1)])
+
+    ops: list[Cmd] = [Pump(reagent="water"), Pump(reagent="gr"), Goto(step=0, n=n - 1)]
+    experiment_auto = Experiment("experiment", False, path=".", cmds=ops, reagents=mix)
+
+    ops = []
+    for i in range(1, n + 1):
+        ops.append(Pump(reagent="water"))
+        ops.append(Pump(reagent=f"antibody{i}"))
+
+    w: Reagents = [Reagent(name="water", port=14)]
+    w += antibodies
+    experiment_man = Experiment(
+        "experiment",
+        False,
+        path=".",
+        cmds=ops,
+        reagents=w,
+    )
+
+    assert experiment_auto.compile() == experiment_man.compile()
+    assert Experiment.parse_raw(experiment_auto.json()) == experiment_auto  # Original object not altered.
+    assert Experiment.parse_raw(experiment_man.json()) == experiment_man
--- tests/test_take_image.py
+++ tests/test_take_image.py
@@ -1,17 +1,13 @@
-#%%
 # Run this in Jupyter or Shift+Enter in VSCode.
 # https://ipython.readthedocs.io/en/stable/interactive/autoawait.html
-
-import asyncio
 
 from pyseq2.imager import Imager
 from pyseq2.utils.log import setup_logger
 from pyseq2.utils.ports import get_ports
 
-setup_logger(level="DEBUG")
 
-
-async def take():
+async def test_take():
+    setup_logger()
     ports = await get_ports()
     imager = await Imager.ainit(ports)
     # await imager.initialize()  # If not initialized in this session (defined by HiSeq power cycle).
@@ -21,11 +17,3 @@ async def take():
     await imager.y.move(1000000)
     img, state = await imager.take(20, dark=True, channels=frozenset((0, 1, 2, 3)))
     await imager.save("test.tiff", img)
-    print("Done!")
-
-
-def test_take():
-    asyncio.run(take())
-
-
-# %%
