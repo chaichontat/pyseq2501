3f9d641651bda4657fc5efe87035c21fa868ceec
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2022-03-02 14:40:08 -0500

    Toggle
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

--- src/components/sidebar/toggle.svelte
+++ src/components/sidebar/toggle.svelte
@@ -1,12 +1,35 @@
 <script lang="ts">
+  import { localStore as ls } from "$src/stores/store";
   import { Switch } from "@rgossiaux/svelte-headlessui";
   export let checked = false;
+
+  const m = $ls.mode;
+  function genColor(x: typeof m) {
+    switch (x) {
+      case "editingA":
+        return "bg-indigo-600";
+      case "editingB":
+        return "bg-purple-600";
+      default:
+        return "bg-blue-700";
+    }
+  }
+
+  $: switch ($ls.mode) {
+    case "editingA":
+      checked = false;
+      break;
+    case "editingB":
+      checked = true;
+      break;
+  }
 </script>
 
 <Switch
   on:change={() => (checked = !checked)}
-  class={`${checked ? "bg-indigo-600" : "bg-blue-700"}
-    relative inline-flex flex-shrink-0 h-[24px] w-[48px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+  class={`${genColor($ls.mode)} ${$ls.mode.startsWith("editing") ? "opacity-40" : ""}
+    relative inline-flex flex-shrink-0 h-[24px] w-[48px] border-2 border-transparent rounded-full cursor-pointer transition-all ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+  disabled={$ls.mode.startsWith("editing")}
 >
   <span
     aria-hidden="true"
