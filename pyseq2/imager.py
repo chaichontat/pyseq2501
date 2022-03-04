0454c1b377e16b85a07900f6ee1b53eacc2f1b9a
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2022-03-04 17:07:26 -0500

    Laplacian
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

--- pyseq2/imager.py
+++ pyseq2/imager.py
@@ -250,7 +250,7 @@ class Imager(metaclass=Singleton):
     def calc_delta_pos(n_px_y: int) -> int:
         return int(n_px_y * Imager.UM_PER_PX * YStage.STEPS_PER_UM)
 
-    async def autofocus(self, channel: Literal[0, 1, 2, 3] = 1) -> tuple[int, UInt16Array]:
+    async def autofocus(self, channel: Literal[0, 1, 2, 3] = 1) -> tuple[int, UInt16Array, UInt16Array]:
         """Moves to z_max and takes 232 (2048 × 5) images while moving to z_min.
         Returns the z position of maximum intensity and the images.
         """
@@ -275,14 +275,24 @@ class Imager(metaclass=Singleton):
                         n_bundles, (height, 4096), fut_capture=start_move, mode=Mode.FOCUS_SWEEP, cam=cam
                     )
 
-            intensity = np.mean(
-                np.reshape(img[CHANNEL[channel] - 2 * cam], (n_bundles, height, 2048)), axis=(1, 2)
-            )
-            target = int(z_max - (((z_max - z_min) / n_bundles) * np.argmax(intensity) + z_min))
+            stack: UInt16Array = np.reshape(img[CHANNEL[channel] - 2 * cam], (n_bundles, height, 2048))
+            intensity = np.mean(stack, axis=(1, 2))
+            var_laplacian = np.var(self.laplacian(stack), axis=(1, 2))
+            cv = var_laplacian / intensity
+            target = int(z_max - (((z_max - z_min) / n_bundles) * np.argmax(cv) + z_min))  # type: ignore
             logger.info(f"Done autofocus. Optimum={target}")
             if not 10000 < target < 50000:
                 logger.info(f"Target too close to edge, considering moving the tilt motors.")
-            return (target, intensity)
+            return (target, cv, stack)
+
+    @staticmethod
+    def laplacian(img: UInt16Array) -> UInt16Array:
+        # https://stackoverflow.com/questions/43086557/convolve2d-just-by-using-numpy
+        k = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
+        shape = k.shape + (img.shape[0],) + tuple(np.subtract(img.shape[1:], k.shape) + 1)
+        strides = (img.strides * 2)[1:]
+        M: UInt16Array = np.lib.stride_tricks.as_strided(img, shape=shape, strides=strides)  # type: ignore
+        return np.einsum("pq,pqbmn->bmn", k, M)
 
     @staticmethod
     def _save(path: str | Path, img: UInt16Array, state: Optional[State] = None) -> None:
