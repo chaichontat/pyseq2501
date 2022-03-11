532e3f7004eaa04dc2efc358728bab68539a0f09
Kunal Pandit <kpandit8387@gmail.com>
2022-03-11 11:28:57 -0500

    fix path errors

--- pyseq2/config.py
+++ pyseq2/config.py
@@ -4,17 +4,21 @@ from typing import Literal
 import yaml
 from pydantic import BaseSettings
 
+pyseq_path = Path.home() / Path('.pyseq')
 
 class Config(BaseSettings):
     machine: Literal["HiSeq2000", "HiSeq2500"] = "HiSeq2000"
-    logPath: str = "~/pyseq2/logs"
+    logPath: str = str(pyseq_path / 'logs')
     logLevel: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "INFO"
 
 
-def load_config(path: Path | str = "~/.pyseq/pyseq.yml") -> Config:
+def load_config(path: Path | str = pyseq_path / 'pyseq.yml') -> Config:
     if isinstance(path, str):
+        print("Found path")
         path = Path(path)
+        print(path)
     if not path.exists():
+        print("Can't find path")
         return Config()
     return Config(**yaml.safe_load(path.read_text()))
 
