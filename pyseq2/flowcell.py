d69b18f24c0a8a480653cbfa5119cf14e7e06f2f
Kunal Pandit <kpandit8387@gmail.com>
2022-03-16 11:41:41 -0400

    fix typos

--- pyseq2/flowcell.py
+++ pyseq2/flowcell.py
@@ -10,7 +10,7 @@ from .fluidics.pump import Pump
 from .fluidics.valve import Valves
 from .utils.log import init_log
 from .utils.utils import Singleton
-import config
+from .config import CONFIG
 
 logger = logging.getLogger(__name__)
 
@@ -76,7 +76,7 @@ class AFlowCell:
         wait: Seconds = 26,
     ) -> None:
 
-        if not in self.enabled_ports:
+        if port not in self.enabled_ports:
             raise ValueError("Invalid port number.")
 
         async with self.arm9chem.shutoff_valve(), self.v.move_port(port), self.lock:
