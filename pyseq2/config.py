63f841c3ace3acecb9942d0709ab73b8a01a3062
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2022-03-16 16:55:42 -0400

    Machine-dependent ports
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

--- pyseq2/config.py
+++ pyseq2/config.py
@@ -1,8 +1,8 @@
 from pathlib import Path
-from typing import Literal
+from typing import Any, Literal
 
 import yaml
-from pydantic import BaseSettings
+from pydantic import BaseSettings, validator
 
 pyseq_path = Path.home() / Path(".pyseq")
 
@@ -11,8 +11,20 @@ class Config(BaseSettings):
     machine: Literal["HiSeq2000", "HiSeq2500"] = "HiSeq2000"
     logPath: str = str(pyseq_path / "logs")
     logLevel: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "INFO"
-    enabled_ports: list[int] = [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
     barrels_per_lane: Literal[1, 2, 4, 8] = 1
+
+    enabled_ports: tuple[int, ...] = None  # type: ignore
+
+    # Hack until https://github.com/samuelcolvin/pydantic/pull/2625 is merged.
+    @validator("enabled_ports", pre=True, always=True)
+    def set_enabled_ports(cls, v: tuple[int, ...], values: dict[str, Any]) -> tuple[int, ...]:
+        match values["machine"]:
+            case "HiSeq2000":
+                return tuple((i for i in range(1, 20) if i != 9))
+            case "HiSeq2500":
+                return tuple(range(1, 25))
+            case _:
+                raise ValueError("Invalid machine.")
 
 
 def load_config(path: Path | str = pyseq_path / "pyseq.yml") -> Config:
