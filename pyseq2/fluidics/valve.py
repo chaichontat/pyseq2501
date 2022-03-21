c8eda508a13782df8ad6bb2d3e93673f406ff274
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2022-03-21 14:57:18 -0400

    Add 2500 test

--- pyseq2/fluidics/valve.py
+++ pyseq2/fluidics/valve.py
@@ -39,7 +39,7 @@ class _Valve(Movable, UsesSerial):
             await self.com.send(ValveCmd.CLEAR_ID)
             if await self.com.send(ValveCmd.ID) != "not used":
                 raise ValveError(f"{name}: Already cleared ID but ID is still here.")
-            if (n := await self.com.send(ValveCmd.GET_N_PORTS)) != self.n_ports:
+            if not IS_FAKE() and (n := await self.com.send(ValveCmd.GET_N_PORTS)) != self.n_ports:
                 raise ValveError(
                     f"{name}: Number of ports is not {self.n_ports} as expected but {n}. Incorrect machine setting?"
                 )
--- tests/test_flowcells.py
+++ tests/test_flowcells.py
@@ -1,9 +1,9 @@
 import asyncio
+from unittest.mock import patch
 
 import pytest
 import pytest_asyncio
 
-from pyseq2.config import CONFIG
 from pyseq2.flowcell import FlowCells
 from pyseq2.utils.ports import get_ports
 
@@ -30,15 +30,21 @@ async def test_valves(fcs: FlowCells):
 
     await asyncio.gather(*[fcs.A.v._move(p) for p in (0, 1, 10)])
 
+    with pytest.raises(ValueError):
+        await fcs.A.v._move(24)
+
     with pytest.raises(NotImplementedError):
         await fcs.A.v.set_fc_inlet(8)
 
-    CONFIG.Config.allow_mutation = True  # Triple very bad. Don't do in production.
-    CONFIG.machine = "HiSeq2500"
-    await fcs.A.v.pos
-    await fcs.A.v.set_fc_inlet(8)
-    await asyncio.gather(*[fcs.A.v._move(p) for p in (0, 1, 10)])
-    CONFIG.machine = "HiSeq2000"
+
+async def test_valves_2500(fcs: FlowCells):
+    with (
+        patch("pyseq2.config.CONFIG.Config.allow_mutation", True),
+        patch("pyseq2.config.CONFIG.machine", "HiSeq2500"),
+    ):
+        await fcs.A.v.pos
+        await fcs.A.v.set_fc_inlet(8)
+        await asyncio.gather(*[fcs.A.v._move(p) for p in (0, 1, 10, 24)])
 
 
 async def test_arm9chem(fcs: FlowCells):
